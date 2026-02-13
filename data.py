001cc98017463ad5ff7c00ebe2ad25a8f97387e5
kirillzavodovskij8-hash <kirillzavodovskij8@gmail.com>
2026-02-13 20:03:13 +0300

    New
    Пропуст

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-db_name = "base"
+db_name = "data_base.db"
 
 def conn():
     return sqlite3.connect(db_name)
@@ -9,14 +9,14 @@ def conn():
 def create():
     con = conn()
     cur = con.cursor()
-    cur.execute(((
-        """CREATE TABLE IF NOT EXISTS table
+    cur.execute
+    ("""CREATE TABLE IF NOT EXISTS table
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT,
         pasword INTEGER,
-        """
-    )))
-
+        """)
+    con.commit()
+    con.close()
 def list_table():
     con = conn()
     cur = con.cursor()
@@ -24,4 +24,15 @@ def list_table():
     a = cur.fetchall()
     for name, password in a:
         print(name, password)
+    con.commit()
     con.close()
+
+def add(name, password):
+    con = conn()
+    cur = con.cursor()
+    cur.execute("INSERT INTO table (name, password) VALUES (?, ?)", (name, password))
+    con.commit()
+    con.close()
+
+conn()
+add("admin", 111)
